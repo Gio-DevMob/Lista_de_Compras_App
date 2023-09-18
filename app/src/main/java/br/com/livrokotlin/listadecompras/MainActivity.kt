e4540589913948e36164b6c711f15179546b23dd
Giovanna Pereira <giovanna.21.mp@gmail.com>
2023-09-17 22:58:13 -0300

    Migrating from Kotlin synthetics to Jetpack view binding

--- app/src/main/java/br/com/livrokotlin/listadecompras/MainActivity.kt
+++ app/src/main/java/br/com/livrokotlin/listadecompras/MainActivity.kt
@@ -3,17 +3,30 @@ package br.com.livrokotlin.listadecompras
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.list_view_produtos
+import br.com.livrokotlin.listadecompras.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // implementação do adaptador
+        // Implementação do adaptador
         val produtosAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+        binding.listViewProdutos.adapter = produtosAdapter
 
-        // definindo o adaptador na lista
-        list_view_produtos.adapter = produtosAdapter
+        // Definição do ouvinte no botão
+        binding.btnInserir.setOnClickListener {
+            // Pegando o valor digitado pelo usuário
+            val produto = binding.txtProduto.text.toString()
+
+            // Enviando o item para a lista
+            produtosAdapter.add(produto)
+
+            // Limpar o campo de texto após a inserção
+            binding.txtProduto.text.clear()
+        }
     }
 }
